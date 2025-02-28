4269f6eb0f65d60d9eadd025859e729dc5626385
mei higashi <mei@meihigashi.com>
2025-02-28 13:30:38 -0800

    preload preload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,16 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+export const fontLoader = (
+  <>
+    <link rel="preload" as="font" href="/fonts/Garmin10.woff2" type="font/woff2" crossOrigin="anonymous" />
+    <link rel="preload" as="font" href="/fonts/Garmin7.woff2" type="font/woff2" crossOrigin="anonymous" />
+    <link rel="preload" as="font" href="/fonts/Garmin5.woff2" type="font/woff2" crossOrigin="anonymous" />
+    <link rel="preload" as="font" href="/fonts/AkkuratMono.woff2" type="font/woff2" crossOrigin="anonymous" />
+    <link rel="preload" as="font" href="/fonts/creep.woff2" type="font/woff2" crossOrigin="anonymous" />
+  </>
+);
+
 export const metadata: Metadata = {
   title: "C:/Users/awhmaisy",
   description: "@awhmaisy",
