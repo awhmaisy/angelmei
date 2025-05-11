2a7b5a311019d58003679bd5d9b98e2705baa97e
mei higashi <mei@meihigashi.com>
2025-05-10 19:28:35 -0700

    5/10 updates

--- app/page.tsx
+++ app/page.tsx
@@ -46,13 +46,13 @@ export default function Home() {
               <br />
               <div className="text-xs font-akkurat">
                 <p className="mb-6">
-                  hi! i&apos;m a social media producer + manager, with expertise in relational engineering && cult building.
+                  hi! i&apos;m a media producer, with expertise in relational engineering && cult building.
                 </p>
                 <p className="mb-6">
                   i dropped out in 3rd grade to homeschool, commit to side quests and observe product/graphic design at <a href="https://www.artcenter.edu/" style={{ color: 'var(--accent)' }}>ACCD</a>. i was configuring a degree at <a href="https://iovine-young.usc.edu/" style={{ color: 'var(--accent)' }}>USC&apos;s iovine and young academy</a> but have since received callings to industrialize zones in ████ + convalesce!
                 </p>
                 <p>
-                  i embed and manufacture exclusively US-made garments for technical companies at <a href="https://mach012.com" style={{ color: 'var(--accent)' }}>MACH-12</a>. 
+                  i also embed and manufacture exclusively US-made garments for technical companies at <a href="https://mach012.com" style={{ color: 'var(--accent)' }}>MACH-12</a>. 
                 </p>
                 <br />
                 <p>────୨ৎ────</p>
@@ -61,10 +61,11 @@ export default function Home() {
                   current fascinations
                 </p>
                 <ul className="list-none space-y-1">
-                  <li>been making lil embeddings <a href="https://are.na/mei-higashi/bymei" className="hover:underline" style={{ color: 'var(--accent)' }}>in ps/ai</a></li>
-                  <li>psyops in the perception of winning</li>
-                  <li>garment distribution & retail experimentation</li>
-                  <li>playing chess - the interplay between human & machine</li>
+                  <li>baby ghostwriting on physics, science and some tech</li>
+                  <li>quantum entanglement of many sorts</li>
+                  <li>dabbling into my reading list</li>
+                  <li>davinci resolve, premiere pro, color grading, oh my!</li>
+                  <li>(still) playing chess - the interplay between human & machine</li>
                 </ul>
               </div>
     
